9ab64d4a33bacdc0c42bce63074a840a6667f2fe
Itallo <gustavofrota06@gmail.com>
2026-05-02 11:51:08 -0300

    atualização

--- aula6_at3.py
+++ aula6_at3.py
@@ -1,3 +1,13 @@
 num = int(input("Digite um número:"))
+print("=" * 60) 
 for i in range(num):
-    print("*" * num) 
+    print("*" * num)
+print("=" * 60) 
+matriz = []
+for i in range(num):
+    lista = []
+    for j in range(num):
+        lista.append('*')
+    matriz.append(lista)
+for lista in matriz:
+    print("".join(lista))
