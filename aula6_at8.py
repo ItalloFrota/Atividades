b11fd9e3bebb72283a6b0ff1171a85321cdb7cf2
Itallo <gustavofrota06@gmail.com>
2026-05-14 22:18:21 -0300

    dificil mas não impossivel

--- aula6_at8.py
+++ aula6_at8.py
@@ -1,5 +1,10 @@
 def sequencia_númerica(num):
     for i in range(1, num + 1):
         print(str(i) * i)
-numero = int(input("Digite um número inteiro: "))
-sequencia_númerica(numero)
+numero = int(input("Digite um número inteiro para ver um piramide de números: "))
+print('=' * 30)
+print(f"Piramide de números até {numero}:")
+print('=' * 30)
+print('Sequência numérica:')
+print('=' * 30)
+sequencia_númerica(numero)   
