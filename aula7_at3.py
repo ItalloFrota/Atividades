e61d8c2bd46d2bab78b398b14c30b0de0ff5960b
ItalloFrota <gustavofrota06@gmail.com>
2026-05-19 20:32:57 -0300

    segundo

--- aula7_at3.py
+++ aula7_at3.py
@@ -1,18 +1,13 @@
 def primos (n):
     if n <= 1:
         return 'Não é primo'
-    if n == 2 or n == 3 or n == 5 or n == 7:
+    elif n == 2:
         return 'É primo'
-    if n % 2 == 0:
-        return 'Não é primo'
-    elif n % 3 == 0:
-        return 'Não é primo'
-    elif n % 5 == 0:
-        return 'Não é primo'
-    elif n % 7 == 0:
-        return 'Não é primo'
-    else:
-        return 'É primo'   
+    for i in range(2, n):
+        if n % i == 0:
+            return 'Não é primo'
+    return 'É primo'
+
 n = int(input("Digite um número: "))
 resultado = primos(n)
 print(resultado)
